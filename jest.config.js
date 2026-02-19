f69d3580559f4e320a22d2d3361ef1d08f35e9fa
aryan <aryansinghrajput1966@gmail.com>
2026-02-19 16:28:22 +0530

    Trigger pipeline

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} */
+module.exports = {
+  testEnvironment: "node",
+
+  transform: {
+    ...tsJestTransformCfg,
+  },
+
+  testMatch: ["**/__tests__/**/*.test.ts"],
+
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+
+  clearMocks: true,
+};
